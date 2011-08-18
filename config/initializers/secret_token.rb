575a57f6e6713c2ab75843d51dee4dececaf5f83
Matteo Folin <matteo.folin@gmail.com>
2011-08-18 11:00:38 +0200

    first commit with site/blog/search/page_images

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+F5lab::Application.config.secret_token = 'ffa03ec55c5ce9cd6a105e39b5afe7c574fc9331ec5bdcdafbaa7d9de8ce44380a63cbc86c743a47281bc5545b9d477406c99a30f12967de84f2a2d8b22b30f0'
