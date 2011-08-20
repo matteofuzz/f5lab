872373636488f8c91c45629e7b784accde605219
Matteo Folin <matteo.folin@gmail.com>
2011-08-20 16:13:24 +0200

    fonts, styles and ...

--- config/initializers/f5lab.rb
+++ config/initializers/f5lab.rb
@@ -1,4 +1,4 @@
 # custom settings
-RefinerySetting.find_or_set(:site_description, "fast and furious web development, with style...")
+RefinerySetting.find_or_set(:site_description, "&ldquo;fast and furious web development, with style&bdquo;")
 RefinerySetting.find_or_set(:show_portfolio_items_in_slideshow, true)
 RefinerySetting.find_or_set(:show_home_images_in_slideshow, false)
