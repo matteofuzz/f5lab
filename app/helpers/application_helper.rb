e0019ba44e30eefdee9733f1b68b0f752ff8e5fb
Matteo Folin <matteo.folin@gmail.com>
2011-09-04 23:02:08 +0200

    style and better theming...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,7 +19,17 @@ module ApplicationHelper
   def theme_switcher            
     tags = ''        
     themes = RefinerySetting.get(:themes) - [ current_theme ]
-    themes.each{|t| tags += content_tag('li', link_to(t+' theme', use_theme_url(t)))}
+    themes.each{|t| tags += content_tag('li', link_to(t+' theme', use_theme_url(t)))}  
+    tags += '<div class="clear"></div>'.html_safe
     content_tag('ul', tags.html_safe, :id => "theme_switcher")
+  end     
+     
+  def logo_link                             
+    desc_tag = content_tag 'span', RefinerySetting.get(:site_description).html_safe, :class => 'site_description'
+    link_to (RefinerySetting.find_or_set(:site_name, "Company Name")+desc_tag).html_safe, root_path
+  end        
+  
+  def favicon_tag
+    '<link rel="icon" href="/favicon.ico" />'.html_safe
   end
 end
